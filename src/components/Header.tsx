e682be94789b9da537be129b67d0b6551c2e7f03
krishna-sm <krishna.web18@gmail.com>
2024-09-26 04:31:19 +0530

    full stack ai code generator using mern stack

---
+++ src/components/Header.tsx
@@ -0,0 +1,22 @@
+import { Link } from 'react-router-dom'
+
+const Header = () => {
+  return (
+    <>
+                <nav className="flex justify-between items-center px-24 py-10">
+                    <Link to={'/'} className='text-2xl font-bold'> AiCode</Link>
+
+                    <ul className="flex items-center gap-x-3 text-base">
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Contact</li>
+                        <li>
+                            <Link to={'/code'} className='px-3 py-2 bg-black text-white rounded-md'>Get Started</Link>
+                        </li>
+                    </ul>
+                </nav>
+    </>
+  )
+}
+
+export default Header
